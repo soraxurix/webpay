9e973ee64f2872fab00be87ae06c3422374e144a
soraxurix <71204059+soraxurix@users.noreply.github.com>
2022-02-11 18:49:46 -0600

    probando

--- pagar.php
+++ pagar.php
@@ -1,13 +1,13 @@
 <?php 
 // Iniciamos las librerías
-use Freshwork\Transbank\CertificationBagFactory;  
+/*use Freshwork\Transbank\CertificationBagFactory;  
 use Freshwork\Transbank\TransbankServiceFactory;  
 use Freshwork\Transbank\RedirectorHelper;  
 include "vendor/autoload.php";
 
 $bag = CertificationBagFactory::integrationWebpayNormal();
 
-$plus = TransbankServiceFactory::normal($bag);  
+$plus = TransbankServiceFactory::normal($bag);  */
 // phpinfo();
 echo "Pagando...";
 
